00965466d14354bcb1ea61b1f567d9b3f62ae09a
Ext1se <ponomarevigor1995@gmail.com>
2020-12-09 15:16:50 +0700

    Init.

---
+++ Assets/Modern UI Pack/Scripts/Modal Window/ModalWindowManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Michsky.UI.ModernUIPack
+{
+    public class ModalWindowManager : MonoBehaviour
+    {
+        public bool IsStartOpen = false;
+        private Animator mwAnimator;
+
+        void Start()
+        {
+            mwAnimator = gameObject.GetComponent<Animator>();
+            if (IsStartOpen)
+            {
+                OpenWindow();
+            }
+        }
+
+        public void OpenWindow()
+        {
+            mwAnimator.Play("Fade-in");
+        }
+
+        public void CloseWindow()
+        {
+            mwAnimator.Play("Fade-out");
+        }
+    }
+}
